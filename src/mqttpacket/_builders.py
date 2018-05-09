912c4d399c74b1085345e6221ee34091f1348dad
Jason Litzinger <jlitzingerdev@gmail.com>
2018-05-08 23:22:14 -0600

    _builders: Add tests and validation to ConnectSpec.

--- src/mqttpacket/_builders.py
+++ src/mqttpacket/_builders.py
@@ -5,6 +5,7 @@ See LICENSE for details.
 import struct
 
 import attr
+import six
 
 from . import _packet
 
@@ -12,16 +13,72 @@ _CONNECT_REMAINING_LENGTH = 10
 
 PROTOCOL_NAME = 'MQTT'.encode('utf-8')
 
+def _check_none_or_text(_instance, attribute, value):
+    if value is not None and not isinstance(value, six.text_type):
+        raise TypeError('{} must be None or text'.format(attribute))
+
+def _check_will_message(instance, _attribute, value):
+    if value is not None and instance.will_topic is None:
+        raise ValueError('Will topic must be set with will message')
+
+def _check_will_topic(instance, _attribute, value):
+    if value is not None and instance.will_message is None:
+        raise ValueError('Will message must be set with will topic')
+
+def _check_password(instance, _attribute, value):
+    if value is not None and instance.username is None:
+        raise ValueError('Password requires username.')
+
+def _check_will_qos(instance, _attribute, value):
+    if value != 0x00 and instance.will_topic is None:
+        raise ValueError('Will QOS requires topic/message')
 
 @attr.s
 class ConnectSpec(object):
     """
     Data class for connection related options.
     """
-    username = attr.ib(default=None)
-    password = attr.ib(default=None)
-    will_topic = attr.ib(default=None)
-    will_message = attr.ib(default=None)
+    username = attr.ib(
+        default=None,
+        validator=_check_none_or_text,
+    )
+    password = attr.ib(
+        default=None,
+        validator=[
+            _check_none_or_text,
+            _check_password,
+        ],
+    )
+    will_topic = attr.ib(
+        default=None,
+        validator=[
+            _check_none_or_text,
+            _check_will_topic
+        ],
+    )
+    will_message = attr.ib(
+        default=None,
+        validator=[
+            _check_none_or_text,
+            _check_will_message
+        ]
+    )
+    will_qos = attr.ib(
+        default=0x00,
+        validator=[
+            attr.validators.in_((0x00, 0x01, 0x02)),
+            _check_will_qos,
+        ]
+    )
+
+    def flags(self):
+        """Get the flags for this connect spec."""
+        flags = 0x02
+
+        if self.will_topic:
+            flags |= 0x04
+
+        return flags
 
     def remaining_length(self):
         """Return the length of the connect options."""
@@ -29,14 +86,14 @@ class ConnectSpec(object):
         if self.username:
             rem_len += 2
             rem_len += len(self.username)
-        elif self.password:
+
+        if self.password:
             rem_len += 2
             rem_len += len(self.password)
-        elif self.will_topic:
-            rem_len += 2
+
+        if self.will_topic:
+            rem_len += 4
             rem_len += len(self.will_topic)
-        elif self.will_message:
-            rem_len += 2
             rem_len += len(self.will_message)
 
         return rem_len
@@ -85,5 +142,4 @@ def connect(client_id, keepalive=60, connect_spec=None):
         parts.append(connect_spec.payload())
 
     msg = b''.join(parts)
-    #TODO: Will Topic, Will Message, User Name, Password
     return msg
--- tests/test_building.py
+++ tests/test_building.py
@@ -2,6 +2,8 @@
 Copyright 2018 Jason Litzinger
 See LICENSE for details.
 """
+import pytest
+
 import mqttpacket
 
 
@@ -13,4 +15,78 @@ def test_connect_basic():
     assert isinstance(packet, bytes)
     assert len(packet) == 20
     assert packet[0] == 16
+    assert packet[9] == 0x02
     assert packet[14:].decode('utf-8') == u'Foobar'
+
+
+def test_will_requirements():
+    """
+    Will topic and will message must be set together.
+    """
+    with pytest.raises(ValueError):
+        mqttpacket.ConnectSpec(
+            will_topic=u'foo',
+        )
+
+    with pytest.raises(ValueError):
+        mqttpacket.ConnectSpec(
+            will_message=u'my message',
+        )
+
+    cs = mqttpacket.ConnectSpec(
+        will_topic=u'my_will_topic',
+        will_message=u'my_will_message'
+    )
+
+    assert cs.will_topic == u'my_will_topic'
+    assert cs.will_message == u'my_will_message'
+
+
+def test_default_spec():
+    """
+    A default spec has a remaining length of zero and
+    a clean session.
+    """
+    cs = mqttpacket.ConnectSpec()
+    assert cs.remaining_length() == 0
+    assert cs.flags() == 0x02
+
+
+def test_will_must_be_unicode():
+    """
+    Will topic and will message must be unicode.
+    """
+    with pytest.raises(TypeError):
+        mqttpacket.ConnectSpec(
+            will_topic=b'foo',
+            will_message=u'bar'
+        )
+
+    with pytest.raises(TypeError):
+        mqttpacket.ConnectSpec(
+            will_topic=u'biz',
+            will_message=b'baz'
+        )
+
+def test_will_qos_values():
+    """
+    Will QOS can only be 0 - 2
+    """
+    with pytest.raises(ValueError):
+        mqttpacket.ConnectSpec(
+            will_topic=u'biz',
+            will_message=u'baz',
+            will_qos=3
+        )
+
+    mqttpacket.ConnectSpec(
+        will_topic=u'my_will_topic',
+        will_message=u'my_will_message',
+        will_qos=1
+    )
+
+    mqttpacket.ConnectSpec(
+        will_topic=u'my_will_topic',
+        will_message=u'my_will_message',
+        will_qos=2
+    )
